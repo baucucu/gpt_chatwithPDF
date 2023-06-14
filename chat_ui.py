9ac7f62e648f3d5e4e6197c09cb36788c585ebf2
Alex Raduca <alexandru.raduca@gmail.com>
2023-06-14 14:19:31 +0300

    update

--- chat_ui.py
+++ chat_ui.py
@@ -7,13 +7,13 @@ from streamlit_chat import message
 from langchain.chat_models import ChatOpenAI
 import openai
 from dotenv import load_dotenv
+from usp.tree import sitemap_tree_for_homepage
 
 load_dotenv()
 
 key = os.getenv('OPENAI_API_KEY')
 os.environ['OPENAI_API_KEY'] = key
 openai.api_key = key
-print(key)
 
 llama_debug = LlamaDebugHandler(print_trace_on_end=True)
 callback_manager = CallbackManager([llama_debug])
@@ -22,73 +22,70 @@ llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 llm_predictor = LLMPredictor(llm=llm)
 service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, chunk_size=512, callback_manager=callback_manager)
 
-PATH_TO_DOCS = r'C:\Users\Alexandru Raduca\Proiecte\chatpdf\gpt_chatwithPDF\docs'
-PATH_TO_PDFS = r'C:\Users\Alexandru Raduca\Proiecte\chatpdf\gpt_chatwithPDF\pdfs'
-PATH_TO_INDEXES = r'C:\Users\Alexandru Raduca\Proiecte\chatpdf\gpt_chatwithPDF\indexes'
+PATH_TO_INDEX = r'C:\Users\Alexandru Raduca\Proiecte\chatpdf\gpt_chatwithPDF\index'
+PATH_TO_FILES = r'C:\Users\Alexandru Raduca\Proiecte\chatpdf\gpt_chatwithPDF\files'
 
-if not os.path.exists(PATH_TO_INDEXES):
-    os.makedirs(PATH_TO_INDEXES)
+if not os.path.exists(PATH_TO_INDEX):
+    os.makedirs(PATH_TO_INDEX)
 
-
-# build index from PDF
-def pdf_to_index(file):
-    if not os.path.exists(PATH_TO_INDEXES):
-        os.makedirs(PATH_TO_INDEXES)
-
-        pdf_path = f'{PATH_TO_PDFS}/{file}'
-        save_path = f'{PATH_TO_INDEXES}/{file}'
-    else:
-        pdf_path = f'{PATH_TO_PDFS}/{file}'
-        save_path = f'{PATH_TO_INDEXES}/{file}'
-
-    PDFReader = download_loader('PDFReader')
-    loader = PDFReader()
-    documents = loader.load_data(file=Path(pdf_path))
-    index = GPTVectorStoreIndex.from_documents(documents)
-    index.storage_context.persist(persist_dir=save_path)
+storage_context = StorageContext.from_defaults(persist_dir=PATH_TO_INDEX)
+index = load_index_from_storage(storage_context, service_context=service_context)
 
 
 # build index from docx
-def docx_to_index(file): 
-    if not os.path.exists(PATH_TO_INDEXES):
-        os.makedirs(PATH_TO_INDEXES)
-        docx_path = f'{PATH_TO_DOCS}/{file}'
-        save_path = f'{PATH_TO_INDEXES}/{file}'
-    else:
-        docx_path = f'{PATH_TO_DOCS}/{file}'
-        save_path = f'{PATH_TO_INDEXES}/{file}' 
-
-    DocxReader = download_loader("DocxReader")
-    loader = DocxReader()
-    documents = loader.load_data(file=Path(docx_path))
-    index = GPTVectorStoreIndex.from_documents(documents) 
-    index.storage_context.persist(persist_dir=save_path) 
+def index_file(file): 
+    extension = file.name[-4:]
+        
+    match extension:
+        case 'docx':
+            reader = download_loader("DocxReader")
+            print(file.name)
+        case '.pdf':
+            reader = download_loader("PDFReader")
+    loader = reader()
+    file_path = f'{PATH_TO_FILES}/{file.name}'
+    documents = loader.load_data(file=Path(file_path))
+    
+    for document in documents:
+        index.insert(document)
+    index.storage_context.persist(persist_dir=PATH_TO_INDEX)
 
 
 # build index from website
 def website_to_index(url):
+
     BeautifulSoupWebReader = download_loader("BeautifulSoupWebReader")
     loader = BeautifulSoupWebReader()
-    documents = loader.load_data(urls=[url])
-    print(documents[0])
-    # Create a new index for the website content
-    website_index = GPTVectorStoreIndex.from_documents(documents)
+    try:
+        tree = sitemap_tree_for_homepage(url)
+        pages = tree.all_pages()
+        count = sum(1 for _ in pages)
+        print(f"Found {count} pages")
+        urls = []
+        for page in tree.all_pages():
+            # print(page)
+            urls.append(page.url)
+        print(f"{len(urls)} urls")
 
-    title = documents[0].extra_info["URL"].replace("https://", "").replace("http://", "").replace("/", "")
-    # Save the index to the storage
-    website_index.storage_context.persist(persist_dir=f'{PATH_TO_INDEXES}/{title}')
+        documents = loader.load_data(urls=urls)
+        print(f"{len(documents)} documents")
+        for document in documents:
+            index.insert(document)
+        index.storage_context.persist(persist_dir=PATH_TO_INDEX)
+    except Exception as ex:
+        print(ex)
+        st.write(ex)
 
 
 # query index using GPT
 def query_index(query_u):
-    index_to_use = get_manual()
-    storage_context = StorageContext.from_defaults(persist_dir=f"{PATH_TO_INDEXES}/{index_to_use}")
+    storage_context = StorageContext.from_defaults(persist_dir=PATH_TO_INDEX)
     index = load_index_from_storage(storage_context, service_context=service_context)
     query_engine = index.as_query_engine()
     response = query_engine.query(query_u)
     
     st.session_state.past.append(query_u)
-    st.session_state.generated.append(response.response)   
+    st.session_state.generated.append(response.response)
 
 
 def clear_convo():
@@ -96,15 +93,9 @@ def clear_convo():
     st.session_state['generated'] = []
 
 
-def get_manual():
-    manual = st.session_state['manual']
-    print(manual)
-    return manual
-
-
 def init():
     st.set_page_config(page_title='KnowHub ChatBot', page_icon=':robot_face: ') 
-    st.sidebar.title('Available resources')
+    st.title("KnowHub ChatBot")
 
 
 if __name__ == '__main__':
@@ -134,26 +125,18 @@ if __name__ == '__main__':
         for i in range(len(st.session_state['generated'])-1, -1, -1): 
             message(st.session_state['generated'][i], key=str(i))
             message(st.session_state['past'][i], is_user=True, key=str(i) + "user")
-    
-    manual_names = os.listdir(PATH_TO_INDEXES)
-    manual = st.sidebar. radio("Choose a resource:", manual_names, key='init')
-    st.session_state['manual'] = manual
-    
+
     file = st.file_uploader("Choose a PDF file to index...")
     clicked = st.button('Index File', key='Upload')
     if file and clicked:
-        extension = file.name[-4:]
-        
-        match extension:
-            case 'docx':
-                docx_to_index(file.name)
-                print(file.name)
-            case '.pdf':
-                pdf_to_index(file.name)
+        with st.spinner(text='Indexing file...'):
+            index_file(file)
+        st.success('File indexed successfully!')
     
-    website_input = st.text_input("Enter website URL:")
-    index_website_button = st.button("Index Website")
+    website = st.text_input("Enter website URL:")
+    clicked = st.button("Index Website")
 
-    if website_input and index_website_button:
-        website_to_index(website_input)
-
+    if website and clicked:
+        with st.spinner(text='Indexing website...'):
+            website_to_index(website)
+        st.success('Website indexed successfully!')
